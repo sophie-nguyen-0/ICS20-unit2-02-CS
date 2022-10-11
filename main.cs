6301fb09751ef87b614dbe5285d7fe599cfadced
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-11 18:13:12 +0000

    cs

--- main.cs
+++ main.cs
@@ -7,13 +7,13 @@ using System;
 
 class Program
 {
-    public static void Main (string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine ("dimensions of a rectangle are: 6cm and 7cm");
-        Console.WriteLine ("the perimeter is:" + ((6 + 7) * 2));
-        Console.WriteLine ("the area is:" + (6 * 7));
+        Console.WriteLine("dimensions of a rectangle are: 6cm and 7cm");
+        Console.WriteLine("the perimeter is:" + ((6 + 7) * 2));
+        Console.WriteLine("the area is:" + (6 * 7));
 
-       Console.WriteLine ("\nDone.");
+        Console.WriteLine("\nDone.");
 
-  }
+    }
 }
