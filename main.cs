0d2ad82d859463887d241b3599650836caa786a3
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-11 18:01:12 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+//created by sophie n
+//oct 2020
+//
+//this program does basic math
+
+using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine ("dimensions of a rectangle are: 6cm and 7cm");
+        Console.WriteLine ("the perimeter is:" + ((6 + 7) * 2));
+        Console.WriteLine ("the area is:" + (6 * 7));
+  }
+}
